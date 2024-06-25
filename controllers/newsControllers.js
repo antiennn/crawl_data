3179e5c6e4858562c379f3dbeb7f1ec9bfcebcf0
antiennn <antien2k3@gmail.com>
2024-06-26 02:44:22 +0700

    add aconomy

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,6 +1,7 @@
 const newsModels = require("../models/news");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { request, response } = require("express");
 const ManageNews = {
   addNewFromVnexpress: async (request, response) => {
     try {
@@ -30,5 +31,34 @@ const ManageNews = {
       return response.status(200).json({"detail":"complete"});
     }
   },
+
+  addNewFromVneconomy: async (request,response)=>{
+    try{
+      const url = "https://vneconomy.vn/the-gioi-kinh-te.htm";
+      axios(url).then((res)=>{
+        const html = res.data
+        const $ = cheerio.load(html);
+        const articles = $("article").get().reverse()
+        articles.forEach(async function (element) {
+          const date = $(element).find("time").text().trim()
+          const text = $(element).find("div.story__summary").text().trim();
+          if (text.length != 0 && date.length !=0) {
+            const news = {
+              content: text,
+              date: new Date(date.split("/").reverse().join("-")),
+              source: "vneconomy",
+            };
+            await newsModels.create(news).catch(err => {
+              console.log(err);
+            });
+          }
+        });
+      })
+    }catch(err){
+      return response.status(500).json(err);
+    }finally{
+      return response.status(200).json({"detail":"complete"});
+    }
+  }
 };
 module.exports = ManageNews;
--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -2,5 +2,6 @@ const route = require("express").Router();
 const ManageNews = require("../controllers/newsControllers");
 
 route.get("/vnexpress",ManageNews.addNewFromVnexpress);
+route.get("/vneconomy",ManageNews.addNewFromVneconomy);
 
 module.exports = route;
